736488d849ae6bee3e44c55feecceb616d1db9dd
Virendra <virendra@fixingdots.in>
2025-02-11 17:18:05 +0530

    aws recognition frontend

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,222 @@
-import Image from "next/image";
+'use client';
+import React, { useRef, useState, useEffect } from 'react';
+import Webcam from 'react-webcam';
+import { v4 as uuidv4 } from 'uuid';
+import AWS from 'aws-sdk';
+import axios from 'axios'; 
 
-export default function Home() {
+export default function EmployeeDetails() {
+
+  
+  AWS.config.update({
+    region: process.env.NEXT_PUBLIC_AWS_REGION,
+    accessKeyId: process.env.NEXT_PUBLIC_AWS_ACCESS_KEY,
+    secretAccessKey: process.env.NEXT_PUBLIC_AWS_SECRET_ACCESS_KEY
+  });
+
+  const rekognition = new AWS.Rekognition();
+  const webcamRef = useRef(null);
+  const [uploadResultMessage, setUploadResultMessage] = useState('Please look at the camera');
+  const [isAuth, setAuth] = useState(false);
+  const [employeeId, setEmployeeId] = useState('');
+  const [name, setName] = useState('');
+  const [department, setDepartment] = useState('');
+  const [designation, setDesignation] = useState('');
+  const [phone, setPhone] = useState('');
+  const [email, setEmail] = useState('');
+  const [address, setAdddress] = useState('');
+  const [isTimeout, setTimeoutStatus] = useState(false);
+
+
+  useEffect(() => {
+    // Periodically capture images and attempt face detection
+    const interval = setInterval(() => {
+      if (webcamRef.current && !isAuth) {
+        captureAndSendImage();
+      }
+    }, 5000); // Capture frame every 2 seconds
+
+    return () => clearInterval(interval);
+  }, [webcamRef]);
+
+  
+
+  const captureAndSendImage = async () => {
+    
+    if (!webcamRef.current || isTimeout) return;
+
+    // Capture the image from the webcam
+    const imageSrc = webcamRef.current.getScreenshot();
+    if (!imageSrc) {
+      return;
+    }
+
+    // Convert base64 image to Blob
+    const blob = dataURItoBlob(imageSrc);
+    const visitorImageName = uuidv4();
+    
+    try {
+    
+      const faceDetected = await detectFaceLocally(blob); // Check if face is detected
+      if (faceDetected) {
+        
+          // Upload to AWS S3
+          await fetch(`https://iti80r2th2.execute-api.us-east-1.amazonaws.com/dev/fixhr-visitor-images/${visitorImageName}.jpg`, {
+            method: 'PUT',
+            headers: {
+              'Content-Type': 'image/jpeg',
+            },
+            body: blob,
+          });
+    
+          // Authenticate the visitor
+          const response = await authenticate(visitorImageName);
+          console.log('aws responswe',response);
+          if (response?.Message === 'Success') {
+
+            try {
+              const { data: employee } = await axios.get(`https://dev.fixhr.app/api/face-detection/employee/${response.FaceId}`);
+              console.log('hr db',employee.status);
+              
+              if (employee.status === true) {
+                setAuth(true);
+                setEmployeeId(employee.data.employeeId);
+                setName(employee.data.name);
+                setDepartment(employee.data.department);
+                setDesignation(employee.data.designation);
+                setPhone(employee.data.phone);
+                setEmail(employee.data.email);
+                setAdddress(employee.data.address);
+  
+                setUploadResultMessage(`Hi ${employee.name} , welcome to work!`);
+              } else {
+                setUploadResultMessage('Employee not found.');
+                setAuth(false);
+              }
+            } catch (error) {
+              console.error('Error calling Laravel API:', error);
+            }
+
+          } else {
+            setAuth(false);
+            setUploadResultMessage('Authentication failed.');
+          }
+        
+      } else {
+        console.log('No face detected. Please adjust your position.');
+      }
+    } catch (error) {
+      console.error('Error during face detection or authentication:', error);
+    }
+    
+    
+  };
+
+  const detectFaceLocally = async (imageBlob: Blob) => {
+    const params = {
+      Image: {
+        Bytes: await imageBlob.arrayBuffer(),
+      },
+      Attributes: ['ALL'],
+    };
+  
+    try {
+      const data = await rekognition.detectFaces(params).promise();
+      return data.FaceDetails && data.FaceDetails.length > 0;
+    } catch (err) {
+      console.error("AWS Rekognition face detection failed", err);
+      return false;
+    }
+  };
+  
+
+  async function authenticate(visitorImageName: string) {
+    const requestUrl = `https://iti80r2th2.execute-api.us-east-1.amazonaws.com/dev/employee?` +
+      new URLSearchParams({ objectKey: `${visitorImageName}.jpg` });
+
+    try {
+      const response = await fetch(requestUrl, {
+        method: 'GET',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+        },
+      });
+      return await response.json();
+    } catch (error) {
+      console.error('Error:', error);
+      return null;
+    }
+  }
+
+  // Utility function to convert base64 to Blob
+  const dataURItoBlob = (dataURI: string) => {
+    const byteString = atob(dataURI.split(',')[1]);
+    const mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
+    const ab = new ArrayBuffer(byteString.length);
+    const ia = new Uint8Array(ab);
+    for (let i = 0; i < byteString.length; i++) {
+      ia[i] = byteString.charCodeAt(i);
+    }
+    return new Blob([ab], { type: mimeString });
+  };
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className="flex items-start justify-center min-h-screen p-8 bg-gray-100">
+      <div className="bg-white rounded-2xl shadow-lg p-6 w-full max-w-4xl grid grid-cols-1 sm:grid-cols-3 gap-4">
+        {/* Left Profile Photo - Larger Size */}
+        <div className="flex flex-col items-center sm:items-start sm:col-span-2">
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+        {!isAuth && (
+        <h3 className="ml-20 text-lg font-semibold text-red-500 mb-2 text-center">
+          Please Look at the Camera
+        </h3>
+      )}
+          
+
+          <div className="w-450 h-150 rounded-xl overflow-hidden">
+            
+             <Webcam
+          ref={webcamRef}
+          screenshotFormat="image/jpeg"
+          className="webcam"
+        />
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+
+        {/* Right Basic Details */}
+        {isAuth ? (<div className="sm:col-span-1 flex flex-col justify-center">
+          <h3 className="text-lg font-semibold text-green-500 mb-2">
+            Welcome to Work
+          </h3>
+          <h2 className="text-2xl font-bold text-gray-800">
+            <span> {employeeId} -</span> {name}
+          </h2>
+          <p className="text-sm text-gray-500 mb-4">
+            {designation}
+          </p>
+
+          <div className="grid grid-cols-1 gap-4">
+            <div>
+              <p className="text-sm font-medium text-gray-600">Department</p>
+              <p className="text-base text-gray-900">{department}</p>
+            </div>
+            <div>
+              <p className="text-sm font-medium text-gray-600">Email</p>
+              <p className="text-base text-gray-900">{email}</p>
+            </div>
+            <div>
+              <p className="text-sm font-medium text-gray-600">Phone</p>
+              <p className="text-base text-gray-900">{phone}</p>
+            </div>
+            <div>
+              <p className="text-sm font-medium text-gray-600">Location</p>
+              <p className="text-base text-gray-900">{address}</p>
+            </div>
+          </div>
+        </div>): ''}
+        
+      </div>
     </div>
   );
 }
